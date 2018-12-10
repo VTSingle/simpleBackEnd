031221046dc770f83caf88503903abf54be4cb58
VTSingle <madheader92@gmail.com>
2018-12-10 21:36:36 +0200

    front

--- app.js
+++ app.js
@@ -94,8 +94,8 @@ app.post('/groupUpdate', function (req, res) {
     });
 });
 
-app.listen(8080, function () {  
-    console.log('Node app is running on port 8080');
+app.listen(3000, function () {
+    console.log('Node app is running on port 3000');
 });
 
 module.exports = app;
