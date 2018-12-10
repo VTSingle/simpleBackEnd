403f092648d3051da46008cb5de9a8f240041427
VTSingle <madheader92@gmail.com>
2018-12-10 23:38:35 +0200

    front

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require('path');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join('../simpleAdminPanel/', 'build')));
+app.use(express.static(path.join('../simpleAdminPanel', 'build')));
 
 const mc = mysql.createConnection({
     host: '127.0.0.1',
