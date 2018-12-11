a4b681e4078b79fb9f6c2188c873b7a82f33b013
VTSingle <madheader92@gmail.com>
2018-12-11 10:50:11 +0200

    Default Changelist

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require('path');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join('../simpleAdminPanel', 'build')));
+// app.use(express.static(path.join('../simpleAdminPanel', 'build')));
 
 const port = process.env.PORT || 5000;
 
@@ -97,8 +97,10 @@ app.post('/groupUpdate', function (req, res) {
     });
 });
 
-app.listen(port, function () {
-    console.log('Node app is running on port ' + port);
-});
+app.use(express.static(path.join(__dirname,'../simpleAdminPanel',  'build')))
+    .get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../simpleAdminPanel', 'build/index.html'));
+    })
+    .listen(port, () => console.log(`Listening on ${ port }`))
 
 module.exports = app;
