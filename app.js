7323e644c7cf58058711de0467e44e66de14c829
VTSingle <madheader92@gmail.com>
2019-05-05 23:09:31 +0300

    front

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 const mc = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: 'makememinet',
+    password: '1111',
     database: 'cmpsdb'
 });
 
